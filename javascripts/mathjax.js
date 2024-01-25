881a7f0d6f30521216520b7858cb5ff5b6dda3c1
ypy <1980878786@qq.com>
2024-01-25 14:51:05 +0800

    site

---
+++ javascripts/mathjax.js
@@ -0,0 +1,21 @@
+window.MathJax = {
+    loader: {load: ['[tex]/color']}, //\color标签
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true,
+      packages: {'[+]': ['color']} //\color标签
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  document$.subscribe(() => { 
+    MathJax.startup.output.clearCache()
+    MathJax.typesetClear()
+    MathJax.texReset()
+    MathJax.typesetPromise()
+  })
